f51f8206f3966e0f6fe7170946d5928c28a67e0b
Sudthipod Wanaprom <sudthipod.w@kkumail.com>
2023-12-03 13:03:49 +0700

    Commented

--- Hello World/Hello.cpp
+++ Hello World/Hello.cpp
@@ -1,12 +1,15 @@
-#include <iostream>
+#include <iostream> // To output to the console (io = Input & Output)
 
-using namespace std;
+using namespace std; // Standard namespace (Implicitly includes std::)
 
+// Main function
 int main()
 {
-    for (int i = 0; i < 3; i++)
-    {
-        cout << "Hello World!" << endl;
+    // Loop 3 times
+    for (int i = 0; i < 3; i++) // i = 0, 1, 2
+    {   
+        string isS = (i > 0) ? "s" : ""; // Ternary operator if there are more than 1 world
+        cout << "Hello " + to_string(i+1) + " World" + isS + "!" << endl; // Concatenate for more the output
     }
-    return 0;
+    return 0; // Return 0 because the function required to return an integer
 }
